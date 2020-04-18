61615fac5c01336df7c8d18566708b49eba12d5c
Viet Hua <huaviet999@gmail.com>
2020-04-18 11:02:09 +0700

    routes example

--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutterexample/pages/detail.dart';
+import 'package:flutterexample/pages/home.dart';
 import 'my_item.dart';
 
 void main() {
@@ -9,53 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/home',
       title: 'Flutter Example',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('List View Example'),
-          centerTitle: true,
-        ),
-        body: Container(
-          child: MyListView(),
-        ),
-      ),
+      routes: {
+        '/home' : (context) => Home(),
+        '/detail': (context) => Detail()
+      },
     );
-  }
-}
-
-class MyListView extends StatefulWidget {
-  @override
-  _MyListViewState createState() => _MyListViewState();
-}
-
-class _MyListViewState extends State<MyListView> {
-  List<MyItem> myItemList = [
-    MyItem.fromTitle('A Title'),
-    MyItem.fromTitle('B Title'),
-    MyItem.fromTitle('C Title'),
-    MyItem.fromTitle('D Title')
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: myItemList.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundImage: AssetImage('images/a.jpg'),
-              ),
-              contentPadding: EdgeInsets.all(10),
-              title: Text(
-                myItemList[index].title,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
-              onTap: () {
-                print('${myItemList[index].title} Clicked');
-              },
-            ),
-          );
-        });
   }
 }
